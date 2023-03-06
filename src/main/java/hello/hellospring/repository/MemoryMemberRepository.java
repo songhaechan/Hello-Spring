3eca334dd8e297b4e7f23a02ff10c27583f6926b
Chan <bukak2019@naver.com>
2023-03-06 16:51:39 +0900

    HomeController , MemberController update

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
@@ -10,7 +11,6 @@ public class MemoryMemberRepository implements MemberRepository{
     private static long sequence = 0L;
 
     @Override
-
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
